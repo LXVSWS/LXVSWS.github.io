be7a337bb9744eef1577809ecf3bf4a811f76c5f
LXV <contact@lxvsws.com>
2021-12-31 00:49:26 +0100

    .save.

--- js/app.js
+++ js/app.js
@@ -4,12 +4,3 @@ btn.onclick = function () {
     menu.classList.toggle("is-active")
     btn.classList.toggle("is-active")
 };
-
-var theme1 = document.getElementById("theme1");
-var theme2 = document.getElementById("theme2");
-theme1.onclick = function () {
-    document.getElementsByTagName("main")[0].removeAttribute("id");
-};
-theme2.onclick = function () {
-    document.getElementsByTagName("main")[0].setAttribute("id", "bg");
-};
