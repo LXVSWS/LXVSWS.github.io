73a23c704ba489669867e6a76910669c325a4d82
LXV <contact@lxvsws.com>
2021-12-31 03:52:13 +0100

    one page

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
 var btn = document.getElementById("btn");
 var menu = document.getElementById("menu");
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
+var modal1 = document.getElementById("modal1");
 
 btn.onclick = function () {
     menu.classList.toggle("is-active")
     btn.classList.toggle("is-active")
+};
+
+btn1.onclick = function () {
+    modal1.classList.toggle("is-active")
+};
+
+btn2.onclick = function () {
+    modal1.classList.toggle("is-active")
+};
+
+btn3.onclick = function () {
+    modal1.classList.toggle("is-active")
+};
+
+btn4.onclick = function () {
+    modal1.classList.toggle("is-active")
 };
