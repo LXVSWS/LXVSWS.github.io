08d27e667c3e569b9de33788d88a9d969ecef1c3
LXV <contact@lxvsws.com>
2022-01-06 05:52:41 +0100

    .save.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,12 @@ var btn1 = document.getElementById("btn1");
 var btn2 = document.getElementById("btn2");
 var btn3 = document.getElementById("btn3");
 var btn4 = document.getElementById("btn4");
+var btn5 = document.getElementById("btn5");
+var btn6 = document.getElementById("btn6");
 var modal1 = document.getElementById("modal1");
+var modal2 = document.getElementById("modal2");
+var onglet = document.getElementById("onglet");
+var onglet2 = document.getElementById("onglet2");
 
 btn.onclick = function () {
     menu.classList.toggle("is-active")
@@ -24,5 +29,21 @@ btn3.onclick = function () {
 };
 
 btn4.onclick = function () {
-    modal1.classList.toggle("is-active")
+    modal2.classList.toggle("is-active")
+};
+
+btn5.onclick = function () {
+    modal2.classList.toggle("is-active")
+};
+
+btn6.onclick = function () {
+    modal2.classList.toggle("is-active")
+};
+
+onglet.onclick = function () {
+    window.open("vulgarisation_informatique.pdf");
+};
+
+onglet2.onclick = function () {
+    window.open("https://buy.stripe.com/9AQ4i1b2Z2fP05W5kk");
 };
